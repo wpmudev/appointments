8bdc79e6ca1a64ddcce7d651ee7ddf080a5317b7
vladislav <ve@incsub.com>
2014-12-30 08:12:33 +0100

    Refactor main appointments list template.

--- includes/class_app_codec.php
+++ includes/class_app_codec.php
@@ -206,6 +206,25 @@ SITE_NAME
 		}
 
 	}
+
+	public static function admin_appointments_list () {
+		$file = self::_to_template_file('appointment_list');
+		if (!empty($file)) require_once($file);
+	}
+	
+	public static function admin_my_appointments_list ($type='active') {
+		$file = self::_to_template_file('my_appointments_list');
+		if (!empty($file)) require_once($file);
+	}
+
+	private static function _to_template_file ($fragment) {
+		if (empty($fragment)) return false;
+		$file = APP_PLUGIN_DIR . '/includes/templates/app-admin-' . preg_replace('/[^-_a-z0-9]/i', '', $fragment) . '.php';
+		return file_exists($file)
+			? $file
+			: false
+		;
+	}
 }
 
 
