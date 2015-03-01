356d82709784ec8db3d36d8d5bb0492de65ae595
vladislav <ve@incsub.com>
2015-03-01 08:34:03 +0100

    Update the minor version number.

--- appointments.php
+++ appointments.php
@@ -3,7 +3,7 @@
 Plugin Name: Appointments+
 Description: Lets you accept appointments from front end and manage or create them from admin side
 Plugin URI: http://premium.wpmudev.org/project/appointments-plus/
-Version: 1.4.7-BETA-8
+Version: 1.4.7
 Author: WPMU DEV
 Author URI: http://premium.wpmudev.org/
 Textdomain: appointments
@@ -32,7 +32,7 @@ if ( !class_exists( 'Appointments' ) ) {
 
 class Appointments {
 
-	var $version = "1.4.7-BETA-8";
+	var $version = "1.4.7";
 
 	function __construct() {
 
