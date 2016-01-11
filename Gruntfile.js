c542a30b3cf30c63878a3fff8d308aa02e71af45
Ignacio Cruz <igmoweb@gmail.com>
2016-01-11 17:30:57 +0100

    Version release

--- Gruntfile.js
+++ Gruntfile.js
@@ -2,9 +2,9 @@ module.exports = function(grunt) {
     grunt.initConfig({
         pot: {
             options: {
-                text_domain: 'appointments', //Your text domain. Produces my-text-domain.pot
-                dest: 'languages/', //directory to place the pot file
-                keywords: [ //WordPress localisation functions
+                text_domain: 'appointments',
+                dest: 'languages/',
+                keywords: [
                     '__:1',
                     '_e:1',
                     '_x:1,2c',
@@ -21,41 +21,18 @@ module.exports = function(grunt) {
                     '_nx_noop:1,2,3c',
                     '__ngettext:1,2',
                     '__ngettext_noop:1,2'
-                ]
+                ],
+                encoding: 'UTF-8',
+                language: 'PHP'
             },
             files: {
-                src: ['**/*.php'], //Parse all php files
+                src: ['**/*.php'],
                 expand: true
-            }
-        },
-        checktextdomain: {
-            options:{
-                text_domain: 'my-plugin',
-                correct_domain: true, //Will correct missing/variable domains
-                keywords: [ //WordPress localisation functions
-                    '__:1,2d',
-                    '_e:1,2d',
-                    '_x:1,2c,3d',
-                    'esc_html__:1,2d',
-                    'esc_html_e:1,2d',
-                    'esc_html_x:1,2c,3d',
-                    'esc_attr__:1,2d',
-                    'esc_attr_e:1,2d',
-                    'esc_attr_x:1,2c,3d',
-                    '_ex:1,2c,3d',
-                    '_n:1,2,4d',
-                    '_nx:1,2,4c,5d'
-                ]
-            },
-            files: {
-                src:  [ '**/*.php', ], //All php files
-                expand: true,
             }
         }
     });
 
     grunt.loadNpmTasks('grunt-pot');
-    grunt.loadNpmTasks('grunt-checktextdomain')
 
     grunt.registerTask('default', ['pot']);
 };
