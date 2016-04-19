3354fda3a7de3e9faea0cc9b4b9a791275e413d6
Ignacio Cruz <igmoweb@gmail.com>
2016-04-19 14:31:18 +0200

    Updated .gitignore Remove build folder

--- build/appointments/admin/js/admin-gcal.js
+++
@@ -1,5 +0,0 @@
-jQuery( document ).ready( function( $ ) {
-    $('.gcal-slider').unslider({
-        nav: false
-    });
-}( jQuery ));
--- build/appointments/includes/templates/app-admin-settings-gcal.php
+++
@@ -1,6 +0,0 @@
-<?php
-global $appointments;
-//if (is_object($appointments->gcal_api)) $appointments->gcal_api->render_tab();
-if ( is_object( $appointments->get_gcal_api() ) ) {
-	$appointments->get_gcal_api()->admin->render_tab();
-}
