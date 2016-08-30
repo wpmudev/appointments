35eeb083b0fe651ec4c99864a8a7cd6ea7ef0162
Ignacio Cruz <igmoweb@gmail.com>
2016-08-30 08:24:43 +0200

    Fixed MarketPress fatal error. New release

--- includes/addons/app-mp-product_cart_appointments.php
+++ includes/addons/app-mp-product_cart_appointments.php
@@ -13,6 +13,7 @@ class App_Mp_ProductCartDisplay {
 
 	/** @var  Appointments */
 	private $_core;
+	private $_data = array();
 	private $_has_marketpress = false;
 
 	private function __construct () {}
@@ -26,101 +27,54 @@ class App_Mp_ProductCartDisplay {
 		add_action('plugins_loaded', array($this, 'initialize'));
 
 		add_filter('app_mp_product_name_in_cart', array($this, 'apply_changes'), 10, 5);
-		add_action( 'app-settings-payment_settings-marketpress', array( $this, 'show_settings' ) );
-	}
 
-<<<<<<< HEAD
-	/**
-	 * Add default MP options to Appointments options
-	 *
-	 * @param array $options
-	 *
-	 * @return array
-	 */
-	public function default_options( $options ) {
-		$mp_options = array(
-			'cart_name_format' => '',
-			'auto_add_to_cart' => false,
-		);
-		return array_merge( $options, $mp_options );
-=======
 		add_action('app-settings-payment_settings-marketpress', array($this, 'show_settings'));
 		add_filter('app-options-before_save', array($this, 'save_settings'));
 
 		add_action( 'wp_ajax_mp_update_cart', array( $this, 'update_apps_on_cart_change' ) );
 		add_action( 'wp_ajax_nopriv_mp_update_cart', array( $this, 'update_apps_on_cart_change' ) );
-
-		add_action( 'app_remove_expired', array( $this, 'remove_app_from_cart_when_expired' ), 10, 2 );
-
-		add_filter( 'mp_product/title', array( $this, 'mp_product_title' ), 2, 2 );
-
->>>>>>> 7b59c5d0734d9cd2d4edcb2a04ebe5cffa48f1fd
 	}
 
 	public function apply_changes ($name, $service, $worker, $start, $app) {
-		$options = appointments_get_options();
-		if ( empty( $options['cart_name_format'] ) ) {
-			return $name;
-		}
+		if (empty($this->_data['cart_name_format'])) return $name;
 		$codec = new App_Macro_Codec($app);
-		return $codec->expand($options['cart_name_format'], App_Macro_Codec::FILTER_TITLE);
+		return $codec->expand($this->_data['cart_name_format'], App_Macro_Codec::FILTER_TITLE);
 	}
 
 	public function auto_add_to_cart () {
-			global $post;
-			if (!$this->_core->is_app_mp_page($post)) return false;
-			?>
-<script>
-(function ($) {
+		global $post;
+		if (!$this->_core->is_app_mp_page($post)) return false;
+		?>
+		<script>
+			(function ($) {
 
-$(document).on("app-confirmation-response_received", function (e, response) {
-	if (!(response && response.mp && 1 == response.mp)) return false;
-	$(".mp_buy_form").hide().submit();
-});
+				$(document).on("app-confirmation-response_received", function (e, response) {
+					if (!(response && response.mp && 1 == response.mp)) return false;
+					$(".mp_buy_form").hide().submit();
+				});
 
-})(jQuery);
-</script>
+			})(jQuery);
+		</script>
 		<?php
 	}
 
 	public function initialize () {
 		global $appointments;
-		$this->_core            = $appointments;
-		$this->_has_marketpress = class_exists( 'MarketPress' );
+		$this->_core = $appointments;
+		$this->_data = $appointments->options;
+		$this->_has_marketpress = class_exists('MarketPress');
 
-		$options = appointments_get_options();
-
-		if ( $this->_has_marketpress && ! empty( $options['auto_add_to_cart'] ) ) {
-			if ( defined( 'MP_VERSION' ) && version_compare( MP_VERSION, '3.0', '<' ) ) {
-				add_action( 'wp_footer', array( $this, 'auto_add_to_cart' ) );
-			}
+		if ($this->_has_marketpress && !empty($this->_data['auto_add_to_cart'])) {
+			if (defined('MP_VERSION') && version_compare(MP_VERSION, '3.0', '<')) add_action('wp_footer', array($this, 'auto_add_to_cart'));
 		}
 	}
 
-	public function show_settings() {
-		$options = appointments_get_options();
-		$codec  = new App_Macro_Codec;
-		$macros = join( '</code>, <code>', $codec->get_macros() );
-		?>
-		<tr>
-			<th scope="row"><label for="app-cart_name_format"><?php _e('Appointment in shopping cart format', 'appointments'); ?></label></th>
-			<td colspan="2">
-				<input type="text" class="widefat" name="cart_name_format" id="app-cart_name_format" value="<?php echo esc_attr( $options['cart_name_format'] ); ?>" />
-				<span class="description"><?php printf(__('You can use these macros: <code>%s</code>', 'appointments'), $macros); ?></span>
-			</td>
-		</tr>
-		<tr>
-			<th scope="row"><label for="app-auto_add_to_cart"><?php _e('Auto-add appointments into cart', 'appointments'); ?></label></th>
-			<td colspan="2">
-				<input type="hidden" name="auto_add_to_cart" value="" />
-				<input type="checkbox" name="auto_add_to_cart" id="app-auto_add_to_cart" value="1" <?php checked( $options['auto_add_to_cart'] ); ?> />
-			</td>
-		</tr>
-		<?php
+	public function save_settings ($options) {
+		if (!empty($_POST['cart_name_format'])) $options['cart_name_format'] = wp_strip_all_tags(stripslashes_deep($_POST['cart_name_format']));
+		$options['auto_add_to_cart'] = !empty($_POST['auto_add_to_cart']);
+		return $options;
 	}
 
-<<<<<<< HEAD
-=======
 	public function show_settings () {
 		if ($this->_has_marketpress) {
 			$codec = new App_Macro_Codec;
@@ -128,20 +82,20 @@ $(document).on("app-confirmation-response_received", function (e, response) {
 
 			$cart_name_format = isset( $this->_data['cart_name_format'] ) ? $this->_data['cart_name_format'] : '';
 			?>
-<tr class="payment_row" <?php if ( $this->_data['payment_required'] != 'yes' ) echo 'style="display:none"';?>>
-	<th scope="row"><?php _e('Appointment in shopping cart format', 'appointments'); ?></th>
-	<td colspan="2">
-		<input type="text" class="widefat" name="cart_name_format" id="app-cart_name_format" value="<?php echo $cart_name_format; ?>" />
-		<span class="description"><?php printf(__('You can use these macros: <code>%s</code>', 'appointments'), $macros); ?></span>
-	</td>
-</tr>
-<tr class="payment_row" <?php if ( $this->_data['payment_required'] != 'yes' ) echo 'style="display:none"';?>>
-	<th scope="row"><?php _e('Auto-add appointments into cart', 'appointments'); ?></th>
-	<td colspan="2">
-		<input type="hidden" name="auto_add_to_cart" value="" />
-		<input type="checkbox" name="auto_add_to_cart" id="app-auto_add_to_cart" value="1" <?php echo (!empty($this->_data['auto_add_to_cart']) ? 'checked="checked"' : ''); ?> />
-	</td>
-</tr>
+			<tr class="payment_row" <?php if ( $this->_data['payment_required'] != 'yes' ) echo 'style="display:none"';?>>
+				<th scope="row"><?php _e('Appointment in shopping cart format', 'appointments'); ?></th>
+				<td colspan="2">
+					<input type="text" class="widefat" name="cart_name_format" id="app-cart_name_format" value="<?php echo $cart_name_format; ?>" />
+					<span class="description"><?php printf(__('You can use these macros: <code>%s</code>', 'appointments'), $macros); ?></span>
+				</td>
+			</tr>
+			<tr class="payment_row" <?php if ( $this->_data['payment_required'] != 'yes' ) echo 'style="display:none"';?>>
+				<th scope="row"><?php _e('Auto-add appointments into cart', 'appointments'); ?></th>
+				<td colspan="2">
+					<input type="hidden" name="auto_add_to_cart" value="" />
+					<input type="checkbox" name="auto_add_to_cart" id="app-auto_add_to_cart" value="1" <?php echo (!empty($this->_data['auto_add_to_cart']) ? 'checked="checked"' : ''); ?> />
+				</td>
+			</tr>
 			<?php
 		}
 	}
@@ -152,14 +106,14 @@ $(document).on("app-confirmation-response_received", function (e, response) {
 		if( mp_get_post_value( 'cart_action' ) != 'remove_item' && mp_get_post_value( 'cart_action' ) != 'undo_remove_item' ) return;
 
 		$product_id = mp_get_post_value( 'product', null );
-			
+
 		if ( mp_get_post_value( 'cart_action' ) != 'empty_cart' && is_null( $product_id ) ) {
 			wp_send_json_error();
 		}
 
 
 		if ( is_array( $product_id ) ) {
-			
+
 			$product_id = mp_arr_get_value( 'product_id', $product_id );
 
 		}
@@ -179,48 +133,5 @@ $(document).on("app-confirmation-response_received", function (e, response) {
 
 	}
 
-
-	public function remove_app_from_cart_when_expired( $appointment, $new_status ){
-
-		if( !class_exists('Marketpress') ) return;
-
-		//Check if appointment exists in cart		
-		$cart_products = mp_cart()->get_items_as_objects();
-
-		foreach( $cart_products as $product ){
-
-			if( get_post_meta( $product->ID, 'name', true ) == $appointment->ID ){
-
-				mp_cart()->remove_item( $product->ID );
-
-			}
-
-		}
-
-	}
-
-
-	public function mp_product_title( $title, MP_Product $product ) {
-		if ( $product->is_variation() ) {
-			//$product might fail if no post_parent. To be sure get_post
-			$post = get_post($product->ID);
-			$parent = get_post( $post->post_parent );
-			if ( strpos( $parent->post_content, '[app_' ) !== false ) {
-				//this is appointments variant
-
-				$app_id = (int)get_post_meta( $product->ID, 'name', true );
-				if( $app = appointments_get_appointment( $app_id ) ){
-
-					return $this->apply_changes ($title, $app->service, $app->worker, $app->start, $app);
-					
-				}
-
-			}
-		}
-
-		return $title;
-	}
-
->>>>>>> 7b59c5d0734d9cd2d4edcb2a04ebe5cffa48f1fd
 }
 App_Mp_ProductCartDisplay::serve();
