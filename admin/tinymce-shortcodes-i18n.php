f9891f29021b16cbbfacde6983512f889d89f235
Ignacio Cruz <igmoweb@gmail.com>
2016-12-16 14:59:33 +0100

    Fixed: Shortcodes TinyMCE buttons

--- admin/tinymce-shortcodes-i18n.php
+++ admin/tinymce-shortcodes-i18n.php
@@ -10,7 +10,14 @@ foreach ( $registered_shortcodes as $shortcode ) {
 	$instance = App_Shortcodes::get_shortcode_instance( $shortcode );
 	if ( $instance && $instance->name ) {
 		$strings->shortcodes->$shortcode = new stdClass();
-		$strings->shortcodes->$shortcode->defaults = $instance->get_defaults();
+		$_defaults = $instance->get_defaults();
+		$defaults = array();
+		foreach ( $_defaults as $key => $default ) {
+			if ( isset( $default['type'] ) ) {
+				$defaults[ $key ] = $default;
+			}
+		}
+		$strings->shortcodes->$shortcode->defaults = $defaults;
 		$strings->shortcodes->$shortcode->name = $instance->name;
 		$strings->shortcodes->$shortcode->shortcode = $shortcode;
 	}
