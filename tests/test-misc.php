a174e472114f92b70f3421a6db01ed54c09397da
Ignacio Cruz <igmoweb@gmail.com>
2016-12-17 10:51:32 +0100

    Updated unit tests

--- tests/test-misc.php
+++ tests/test-misc.php
@@ -7,55 +7,6 @@
  */
 class App_Misc_Test extends App_UnitTestCase {
 
-	public function test_datepick_local_months() {
-		add_filter( 'locale', function() {
-			return 'es_ES';
-		});
-		$months = appointments()->datepick_local_months();
-		$this->assertCount( 12, $months );
-		$this->assertEquals(
-			array(
-				" Enero",
-				"Febrero",
-				"Marzo",
-				"Abril",
-				"Mayo",
-				"Junio",
-				"Julio",
-				"Agosto",
-				"Septiembre",
-				"Octubre",
-				"Noviembre",
-				"Diciembre"
-			),
-			$months
-		);
-	}
-
-	public function test_datepick_abb_local_months() {
-		add_filter( 'locale', function() {
-			return 'es_ES';
-		});
-		$months = appointments()->datepick_abb_local_months();
-		$this->assertCount( 12, $months );
-		$this->assertEquals(
-			array(
-				" Ene",
-				"Feb",
-				"Mar",
-				"Abr",
-				"May",
-				"Jun",
-				"Jul",
-				"Ago",
-				"Sep",
-				"Oct",
-				"Nov",
-				"Dic"
-			),
-			$months
-		);
-	}
 
 	public function test_weekly_schedule_slots() {
 
