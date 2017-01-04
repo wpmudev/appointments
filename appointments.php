d2c82252ea35de2a521bc17803d3f4b9d6238bff
Ignacio Cruz <igmoweb@gmail.com>
2017-01-04 11:48:29 +0100

    new release

--- appointments.php
+++ appointments.php
@@ -3,7 +3,7 @@
 Plugin Name: Appointments+
 Description: Lets you accept appointments from front end and manage or create them from admin side
 Plugin URI: http://premium.wpmudev.org/project/appointments-plus/
-Version: 1.9.4.2
+Version: 2.0.0
 Author: WPMU DEV
 Author URI: http://premium.wpmudev.org/
 Textdomain: appointments
@@ -32,7 +32,7 @@ if ( !class_exists( 'Appointments' ) ) {
 
 class Appointments {
 
-	public $version = "1.9.4.3";
+	public $version = "2.0";
 	public $db_version;
 
 	public $timetables = array();
