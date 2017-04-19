784c66d8d51eea2c9430bde62474ce996e15bc1e
Ignacio Cruz <igmoweb@gmail.com>
2017-04-19 17:07:52 +0200

    Revert " New: Gulp tasks to handle git operations"
    This reverts commit 6e6fd0a4186ff11b568ddb4c66aee2687711b013.

--- gulpfile.js
+++ gulpfile.js
@@ -2,16 +2,7 @@ var gulp = require('gulp');
 var git = require('gulp-git');
 
 gulp.task('git', function(){
-    // git.checkout('master', function (err) {
-    //     if (err) throw err;
-    // });
-
-    git.checkout( 'wporg-master', function(err) {
-        if ( err ) {
-            throw err;
-        }
-
-        console.log( 'Switched to wporg-master branch' );
-        
+    git.checkout('master', function (err) {
+        if (err) throw err;
     });
 });
