83a1752696865a2a7d0fedee6c69ab20bfff9083
Panos <panos.lyrakis@gmail.com>
2018-03-19 19:35:31 +0200

    Paddings fix

--- includes/helpers.php
+++ includes/helpers.php
@@ -407,19 +407,25 @@ function apppointments_is_range_busy( $start, $end, $args = array() ) {
 
 	// If a specific worker is selected, we will look at his schedule first.
 	if ( 0 != $args['worker_id'] ) {
-		$apps = $appointments->get_reserve_apps_by_worker( $args['location_id'], $args['worker_id'], $week );
-		if ( $apps ) {
-			foreach ( $apps as $app ) {
-				//if ( $start >= strtotime( $app->start ) && $end <= strtotime( $app->end ) ) return true;
-				$app_properties = apply_filters( 'app-properties-for-calendar', array( 'start' => $app->start, 'end' => $app->end ), $app, $args );
-				
-				if ( $period->contains( $app_properties['start'], $app_properties['end'] ) ) {
-					return true;
-				}
-			}
-		}
+
 		if ( ! appointments_is_working( $start, $end, $args['worker_id'], $args['location_id'] ) ) {
 			return true;
+		}
+
+		$apps = $appointments->get_reserve_apps_by_worker( $args['location_id'], $args['worker_id'], $week );
+	}
+	else{
+		$apps = appointments_get_appointments( $args );
+	}
+
+	if ( $apps ) {
+		foreach ( $apps as $app ) {
+			//if ( $start >= strtotime( $app->start ) && $end <= strtotime( $app->end ) ) return true;
+			$app_properties = apply_filters( 'app-properties-for-calendar', array( 'start' => $app->start, 'end' => $app->end ), $app, $args );
+			
+			if ( $period->contains( $app_properties['start'], $app_properties['end'], true ) ) {
+				return true;
+			}
 		}
 	}
 
