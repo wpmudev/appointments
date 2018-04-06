518783b00a219d35cab7fa9c2397e2ec98ac3788
Marcin Pietrzak <marcin@iworks.pl>
2018-04-06 12:52:51 +0200

    change file header

--- appointments.php
+++ appointments.php
@@ -11,21 +11,22 @@ WDP ID: 679841
 */
 
 /*
-Copyright 2007-2013 Incsub (http://incsub.com)
+Copyright 2007-2018 Incsub (http://incsub.com)
 Author - Hakan Evin <hakan@incsub.com>
 Contributor - Ve Bailovity (Incsub)
-This program is free software; you can redistribute it and/or modify
-it under the terms of the GNU General Public License (Version 2 - GPLv2) as published by
-the Free Software Foundation.
+Contributor - Marcin Pietrzak (Incsub)
 
-This program is distributed in the hope that it will be useful,
-but WITHOUT ANY WARRANTY; without even the implied warranty of
-MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
-GNU General Public License for more details.
+This program is free software; you can redistribute it and/or modify it under
+the terms of the GNU General Public License (Version 2 - GPLv2) as published
+by the Free Software Foundation.
 
-You should have received a copy of the GNU General Public License
-along with this program; if not, write to the Free Software
-Foundation, Inc., 59 Temple Place, Suite 330, Boston, MA  02111-1307  USA
+This program is distributed in the hope that it will be useful, but WITHOUT
+ANY WARRANTY; without even the implied warranty of MERCHANTABILITY or FITNESS
+FOR A PARTICULAR PURPOSE.  See the GNU General Public License for more details.
+
+You should have received a copy of the GNU General Public License along with
+this program; if not, write to the Free Software Foundation, Inc., 59 Temple
+Place, Suite 330, Boston, MA  02111-1307  USA
 */
 
 if ( ! class_exists( 'Appointments' ) ) {
