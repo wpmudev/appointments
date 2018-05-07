8ae41d83250c58d3a7b865d0f13bbb09f09727ba
Marcin Pietrzak <marcin@iworks.pl>
2018-05-07 11:14:10 +0200

    missing dots on the end of sentence

--- includes/gcal/views/settings-gcal-step-1.php
+++ includes/gcal/views/settings-gcal-step-1.php
@@ -5,7 +5,7 @@
 	<ul>
 		<li>
 			<img src="<?php echo appointments_plugin_url() . 'images/gcal-instructions-01.png'; ?>" alt="">
-			<p>1. <?php printf( __( 'Go to %s and create a new project. i.e. "Appointments APP", then click "Create"', 'appointments' ), sprintf( '<a target=_blank" href="https://console.developers.google.com/project">%s</a>', __( 'Google Developer Console Projects', 'appointments' ) ) ); ?></p>
+			<p>1. <?php printf( __( 'Go to %s and create a new project. i.e. "Appointments APP", then click "Create".', 'appointments' ), sprintf( '<a target=_blank" href="https://console.developers.google.com/project">%s</a>', __( 'Google Developer Console Projects', 'appointments' ) ) ); ?></p>
 		</li>
 		<li>
 			<img src="<?php echo appointments_plugin_url() . 'images/gcal-instructions-02.png'; ?>" alt="">
@@ -17,7 +17,7 @@
 		</li>
 		<li>
 			<img src="<?php echo appointments_plugin_url() . 'images/gcal-instructions-04.png'; ?>" alt="">
-			<p>4. <?php _e( 'Enable the Calendar API"', 'appointments' ); ?></p>
+			<p>4. <?php _e( 'Enable the Calendar API".', 'appointments' ); ?></p>
 		</li>
 		<li>
 			<img src="<?php echo appointments_plugin_url() . 'images/gcal-instructions-05.png'; ?>" alt="">
@@ -25,15 +25,15 @@
 		</li>
 		<li>
 			<img src="<?php echo appointments_plugin_url() . 'images/gcal-instructions-06.png'; ?>" alt="">
-			<p>6. <?php _e( '... and then "OAuth consent screen" tab. Choose a product name shown to users, i.e. "Appointments +"', 'appointments' ); ?></p>
+			<p>6. <?php _e( '... and then "OAuth consent screen" tab. Choose a product name shown to users, i.e. "Appointments +".', 'appointments' ); ?></p>
 		</li>
 		<li>
 			<img src="<?php echo appointments_plugin_url() . 'images/gcal-instructions-07.png'; ?>" alt="">
-			<p>7. <?php _e( 'Click again on "Credentials" tab and then Create Credentials', 'appointments' ); ?></p>
+			<p>7. <?php _e( 'Click again on "Credentials" tab and then Create Credentials.', 'appointments' ); ?></p>
 		</li>
 		<li>
 			<img src="<?php echo appointments_plugin_url() . 'images/gcal-instructions-08.png'; ?>" alt="">
-			<p>8. <?php _e( 'Select the "Oauth client ID" option', 'appointments' ); ?></p>
+			<p>8. <?php _e( 'Select the "Oauth client ID" option.', 'appointments' ); ?></p>
 		</li>
 		<li>
 			<img src="<?php echo appointments_plugin_url() . 'images/gcal-instructions-09.png'; ?>" alt="">
@@ -41,7 +41,7 @@
 		</li>
 		<li>
 			<img src="<?php echo appointments_plugin_url() . 'images/gcal-instructions-10.png'; ?>" alt="">
-			<p>10. <?php _e( 'Take note of the client ID and client secret and fill the form below', 'appointments' ); ?></p>
+			<p>10. <?php _e( 'Take note of the client ID and client secret and fill the form below.', 'appointments' ); ?></p>
 		</li>
 	</ul>
 </div>
