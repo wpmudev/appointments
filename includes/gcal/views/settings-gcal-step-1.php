181d7a5d0d2af15a334ed48ab07637d1721e2370
Marcin Pietrzak <marcin@iworks.pl>
2018-05-09 11:42:50 +0200

    typos

--- includes/gcal/views/settings-gcal-step-1.php
+++ includes/gcal/views/settings-gcal-step-1.php
@@ -17,7 +17,7 @@
 		</li>
 		<li>
 			<img src="<?php echo appointments_plugin_url() . 'images/gcal-instructions-04.png'; ?>" alt="">
-			<p>4. <?php _e( 'Enable the Calendar API"', 'appointments' ); ?></p>
+			<p>4. <?php _e( 'Enable the "Calendar API".', 'appointments' ); ?></p>
 		</li>
 		<li>
 			<img src="<?php echo appointments_plugin_url() . 'images/gcal-instructions-05.png'; ?>" alt="">
@@ -25,7 +25,7 @@
 		</li>
 		<li>
 			<img src="<?php echo appointments_plugin_url() . 'images/gcal-instructions-06.png'; ?>" alt="">
-			<p>6. <?php _e( '... and then "OAuth consent screen" tab. Choose a product name shown to users, i.e. "Appointments +"', 'appointments' ); ?></p>
+			<p>6. <?php _e( '... and then "OAuth consent screen" tab. Choose a product name shown to users, i.e. "Appointments+"', 'appointments' ); ?></p>
 		</li>
 		<li>
 			<img src="<?php echo appointments_plugin_url() . 'images/gcal-instructions-07.png'; ?>" alt="">
@@ -33,7 +33,7 @@
 		</li>
 		<li>
 			<img src="<?php echo appointments_plugin_url() . 'images/gcal-instructions-08.png'; ?>" alt="">
-			<p>8. <?php _e( 'Select the "Oauth client ID" option', 'appointments' ); ?></p>
+			<p>8. <?php _e( 'Select the "Oauth client ID" option.', 'appointments' ); ?></p>
 		</li>
 		<li>
 			<img src="<?php echo appointments_plugin_url() . 'images/gcal-instructions-09.png'; ?>" alt="">
@@ -41,7 +41,7 @@
 		</li>
 		<li>
 			<img src="<?php echo appointments_plugin_url() . 'images/gcal-instructions-10.png'; ?>" alt="">
-			<p>10. <?php _e( 'Take note of the client ID and client secret and fill the form below', 'appointments' ); ?></p>
+			<p>10. <?php _e( 'Take note of the client ID and client secret and fill the form below.', 'appointments' ); ?></p>
 		</li>
 	</ul>
 </div>
