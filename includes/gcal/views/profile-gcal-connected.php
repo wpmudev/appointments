63d12538f5b3c782e5f29682cb1647740ff9df3e
Marcin Pietrzak <marcin@iworks.pl>
2018-05-15 11:33:31 +0200

    [Bug] Problem with Google Calendar integration https://app.asana.com/0/41436557021226/669539513904499

--- includes/gcal/views/profile-gcal-connected.php
+++ includes/gcal/views/profile-gcal-connected.php
@@ -6,14 +6,20 @@
 			<label for="app-calendar"><?php _e( 'Calendar', 'appointments' ); ?></label>
 		</th>
 		<td>
+            <?php if ( is_array( $calendars ) ) { ?>
 			<select name="gcal_selected_calendar" id="app-calendar">
 				<option value=""><?php _e( '-- Select a Calendar --', 'appointments' ); ?></option>
-				<?php foreach ( $calendars as $calendar ): ?>
+				<?php foreach ( $calendars as $calendar ) :  ?>
 					<option value="<?php echo esc_attr( $calendar['id'] ); ?>" <?php selected( $selected_calendar, $calendar['id'] ); ?>>
 						<?php echo $calendar['summary']; ?>
 					</option>
 				<?php endforeach; ?>
-			</select>
+            </select>
+<?php } else { ?>
+            <div class="notice notice-error inline">
+                <p><?php _e( 'There was an error loading your calendars.', 'appointments' ); ?></p>
+            </div>
+<?php } ?>
 		</td>
 	</tr>
 	<tr>
@@ -34,25 +40,25 @@
 				</option>
 			</select>
 			<br />
-			<span class="description"><?php _e('Select method of integration. A+ -> GCal setting sends appointments to your selected Google calendar, but events in your Google Calendar account are not imported to Appointments+ and thus they do not reserve your available working times. A+ <-> GCal setting works in both directions.', 'appointments') ?></span>
+			<span class="description"><?php _e( 'Select method of integration. A+ -> GCal setting sends appointments to your selected Google calendar, but events in your Google Calendar account are not imported to Appointments+ and thus they do not reserve your available working times. A+ <-> GCal setting works in both directions.', 'appointments' ) ?></span>
 		</td>
 	</tr>
 	<tr>
-		<th scope="row"><label for="gcal_summary"><?php _e('Event summary (name)', 'appointments') ?></label></th>
+		<th scope="row"><label for="gcal_summary"><?php _e( 'Event summary (name)', 'appointments' ) ?></label></th>
 		<td>
 			<input id="gcal_summary" value="<?php echo esc_attr( $gcal_summary ); ?>" size="90" name="gcal_summary" type="text"/>
 		</td>
 	</tr>
 
 	<tr>
-		<th scope="row"><label for="gcal_description"><?php _e('Event description', 'appointments') ?></label></th>
+		<th scope="row"><label for="gcal_description"><?php _e( 'Event description', 'appointments' ) ?></label></th>
 		<td>
 			<textarea rows="6" cols="30" class="widefat" name="gcal_description" id="gcal_description"><?php echo esc_textarea( $gcal_description ); ?></textarea>
 			<br />
 						<span class="description">
-							<?php _e('For the above 2 fields, you can use the following placeholders which will be replaced by their real values:', 'appointments') ?>&nbsp;SITE_NAME, CLIENT, SERVICE, SERVICE_PROVIDER, DATE_TIME, PRICE, DEPOSIT, PHONE, NOTE, ADDRESS, EMAIL <?php _e("(Client's email)", "appointments")?>
+							<?php _e( 'For the above 2 fields, you can use the following placeholders which will be replaced by their real values:', 'appointments' ) ?>&nbsp;SITE_NAME, CLIENT, SERVICE, SERVICE_PROVIDER, DATE_TIME, PRICE, DEPOSIT, PHONE, NOTE, ADDRESS, EMAIL <?php _e( "(Client's email)", 'appointments' )?>
 							<br />
-							<?php _e('Please be careful about privacy if your calendar is public.', 'appointments'); ?>
+							<?php _e( 'Please be careful about privacy if your calendar is public.', 'appointments' ); ?>
 						</span>
 		</td>
 	</tr>
