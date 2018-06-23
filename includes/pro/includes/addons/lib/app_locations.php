e01554e2a54c4afed70dd26137e1cf48b7687982
Marcin Pietrzak <marcin@iworks.pl>
2018-06-23 13:24:57 +0200

    [Bug] Services list just disappeared if added location to the service https://app.asana.com/0/41436557021226/718289412470219:

--- includes/pro/includes/addons/lib/app_locations.php
+++ includes/pro/includes/addons/lib/app_locations.php
@@ -1,6 +1,6 @@
 <?php
 
-if (!defined('APP_LOCATIONS_LOCATION_DEFAULT_MODEL_INSTANCE')) define('APP_LOCATIONS_LOCATION_DEFAULT_MODEL_INSTANCE', 'App_Locations_DefaultLocation', true);
+if ( ! defined( 'APP_LOCATIONS_LOCATION_DEFAULT_MODEL_INSTANCE' ) ) { define( 'APP_LOCATIONS_LOCATION_DEFAULT_MODEL_INSTANCE', 'App_Locations_DefaultLocation', true ); }
 
 /**
  * Class Appointments_Location
@@ -13,7 +13,7 @@ class Appointments_Location {
 	public $address;
 
 	public function __construct( $location ) {
-		$location = (array)$location;
+		$location = (array) $location;
 		$this->id = absint( $location['id'] );
 		$this->address = $location['address'];
 	}
@@ -28,32 +28,26 @@ class Appointments_Location {
  */
 function appointments_insert_location( $args ) {
 	$defaults = array(
-		'address' => ''
+		'address' => '',
 	);
 	$args = wp_parse_args( $args, $defaults );
-
 	$id = rand( 0, 99 );
 	$appx = '';
 	$count = 0;
-	while ($count < 3) {
-		$appx .= sprintf('%04d', rand(0,99));
-		if (intval("{$id}{$appx}") + 5 >= PHP_INT_MAX) break;
+	while ( $count < 3 ) {
+		$appx .= sprintf( '%04d', rand( 0,99 ) );
+		if ( intval( "{$id}{$appx}" ) + 5 >= PHP_INT_MAX ) { break; }
 		$id .= $appx;
 		$count++;
 	}
-	$id = intval($id);
-
+	$id = intval( $id );
 	$locations = get_option( 'app_locations_data', array() );
-
 	$locations[] = array(
 		'id' => $id,
-		'address' => $args['address']
+		'address' => $args['address'],
 	);
-
 	update_option( 'app_locations_data', $locations );
-
 	return $id;
-
 }
 
 /**
@@ -102,7 +96,6 @@ function appointments_delete_location( $id ) {
 			break;
 		}
 	}
-
 	$locs = array_values( $locs );
 	update_option( 'app_locations_data', $locs );
 }
@@ -120,12 +113,10 @@ function appointments_update_location( $id, $args ) {
 	if ( ! $location ) {
 		return false;
 	}
-
 	$defaults = array(
-		'address' => $location->address
+		'address' => $location->address,
 	);
 	$args = wp_parse_args( $args, $defaults );
-
 	$locs = get_option( 'app_locations_data', array() );
 	$location = false;
 	foreach ( $locs as $key => $loc ) {
@@ -134,18 +125,14 @@ function appointments_update_location( $id, $args ) {
 			break;
 		}
 	}
-
 	if ( $location !== false ) {
 		foreach ( $args as $arg => $value ) {
 			$locs[ $location ][ $arg ] = $value;
 		}
-
 		update_option( 'app_locations_data', $locs );
 		return true;
 	}
-
 	return false;
-
 }
 
 abstract class App_Locations_Location {
@@ -155,10 +142,9 @@ abstract class App_Locations_Location {
 
 	protected $_data = array();
 
-
-	abstract public function to_location ($data);
+	abstract public function to_location ( $data );
 	abstract public function to_storage ();
-	abstract public function get_display_markup ($rich_content=true);
+	abstract public function get_display_markup ( $rich_content = true );
 	abstract public function get_admin_label ();
 
 	/**
@@ -166,50 +152,52 @@ abstract class App_Locations_Location {
 	 *
 	 * @return App_Locations_Location
 	 */
-	public static function get_location ($data) {
-		$class = apply_filters('app-locations-location-model_instance_class', APP_LOCATIONS_LOCATION_DEFAULT_MODEL_INSTANCE);
-		$class = $class && class_exists($class) ? $class : APP_LOCATIONS_LOCATION_DEFAULT_MODEL_INSTANCE;
+	public static function get_location( $data ) {
+		$class = apply_filters( 'app-locations-location-model_instance_class', APP_LOCATIONS_LOCATION_DEFAULT_MODEL_INSTANCE );
+		$class = $class && class_exists( $class ) ? $class : APP_LOCATIONS_LOCATION_DEFAULT_MODEL_INSTANCE;
 		/** @var App_Locations_Location $instance */
 		$instance = new $class;
-		$instance->to_location($data);
+		$instance->to_location( $data );
 		return $instance;
 	}
 
-	public function get ($field) {
-		if (!empty($this->_data[$field])) return $this->_data[$field];
+	public function get( $field ) {
+		if ( ! empty( $this->_data[ $field ] ) ) {
+			return $this->_data[ $field ];
+		}
 		return false;
 	}
 
-	public function get_var ($key, $fallback=false) {
-		return !empty($this->_data[$key])
-			? $this->_data[$key]
+	public function get_var( $key, $fallback = false ) {
+		return ! empty( $this->_data[ $key ] )
+			? $this->_data[ $key ]
 			: $fallback
-		;
-	}
-	
-	public function set_var ($key, $value) {
-		$this->_data[$key] = $value;
+			;
 	}
 
-	// Not using uniqid or hashes as we need to place 
+	public function set_var( $key, $value ) {
+		$this->_data[ $key ] = $value;
+	}
+
+	// Not using uniqid or hashes as we need to place
 	// the reference to this in forcefully INT type
-	protected function _create_id () {
-		$id = sprintf('%d', rand(0,99));
+	protected function _create_id() {
+		$id = sprintf( '%d', rand( 0,99 ) );
 		$appx = '';
 		$count = 0;
-		while ($count < 3) {
-			$appx .= sprintf('%04d', rand(0,99));
-			if (intval("{$id}{$appx}") + 5 >= PHP_INT_MAX) break;
+		while ( $count < 3 ) {
+			$appx .= sprintf( '%04d', rand( 0,99 ) );
+			if ( intval( "{$id}{$appx}" ) + 5 >= PHP_INT_MAX ) { break; }
 			$id .= $appx;
 			$count++;
 		}
-		return intval($id);
+		return intval( $id );
 	}
 }
 
 class App_Locations_DefaultLocation extends App_Locations_Location {
 
-	public function to_location ($data) {
+	public function to_location( $data ) {
 		$data = wp_parse_args($data, array(
 			self::KEY_ID => $this->_create_id(),
 			self::KEY_ADDRESS => '',
@@ -217,20 +205,22 @@ class App_Locations_DefaultLocation extends App_Locations_Location {
 		$this->_data = $data;
 	}
 
-	public function to_storage () {
+	public function to_storage() {
 		return $this->_data;
 	}
 
-	public function get_display_markup ($rich_content=true) {
+	public function get_display_markup( $rich_content = true ) {
 		return $this->get_address();
 	}
 
-	public function get_admin_label () {
+	public function get_admin_label() {
 		return $this->get_address();
 	}
 
-	public function get_id () { return $this->get_var(self::KEY_ID); }
-	public function get_address () { return $this->get_var(self::KEY_ADDRESS); }
+	public function get_id() {
+		return $this->get_var( self::KEY_ID ); }
+	public function get_address() {
+		return $this->get_var( self::KEY_ADDRESS ); }
 }
 
 class App_Locations_Model {
@@ -244,56 +234,56 @@ class App_Locations_Model {
 	 */
 	private static $_instance;
 
-	private function __construct () {
+	private function __construct() {
 		$this->reload();
 	}
 
-	public static function get_instance () {
-		if (self::$_instance) return self::$_instance;
+	public static function get_instance() {
+		if ( self::$_instance ) { return self::$_instance; }
 		self::$_instance = new App_Locations_Model;
-		add_action('app-locations-location-model-item_updated', array(self::$_instance, 'update'));
+		add_action( 'app-locations-location-model-item_updated', array( self::$_instance, 'update' ) );
 		return self::$_instance;
 	}
 
-	public function reload () {
-		$data = get_option(self::STORAGE_AREA, array());
-		$this->populate_from_storage($data);
+	public function reload() {
+		$data = get_option( self::STORAGE_AREA, array() );
+		$this->populate_from_storage( $data );
 	}
 
-	public function populate_from_storage ($data) {
+	public function populate_from_storage( $data ) {
 		$this->_data = array();
-		foreach ($data as $location) {
-			$this->_data[] = App_Locations_Location::get_location($location);
+		foreach ( $data as $location ) {
+			$this->_data[] = App_Locations_Location::get_location( $location );
 		}
 	}
 
-	public function update () {
+	public function update() {
 		$data = array();
-		foreach ($this->_data as $location) {
+		foreach ( $this->_data as $location ) {
 			/** @var App_Locations_Location $location */
 			$data[] = $location->to_storage();
 		}
-		update_option(self::STORAGE_AREA, $data);
+		update_option( self::STORAGE_AREA, $data );
 		$this->reload();
 	}
 
-	public function find_by ($field, $value) {
-		foreach ($this->_data as $location) {
+	public function find_by( $field, $value ) {
+		foreach ( $this->_data as $location ) {
 			/** @var App_Locations_Location $location */
-			if ($location->get($field) == $value) return $location;
+			if ( $location->get( $field ) == $value ) { return $location; }
 		}
 		return false;
 	}
 
-	public function index_by ($field, $value) {
-		foreach ($this->_data as $idx => $location) {
+	public function index_by( $field, $value ) {
+		foreach ( $this->_data as $idx => $location ) {
 			/** @var App_Locations_Location $location */
-			if ($location->get($field) == $value) return $idx;
+			if ( $location->get( $field ) == $value ) { return $idx; }
 		}
 		return false;
 	}
 
-	public function get_all () {
+	public function get_all() {
 		return $this->_data;
 	}
 }
@@ -307,13 +297,17 @@ class App_Locations_MappedLocation extends App_Locations_Location {
 	private $_maps_model;
 	private $_maps_codec;
 
-	public function __construct () {
-		if (!class_exists('AgmMapModel')) return false;
+	public function __construct() {
+		if ( ! class_exists( 'AgmMapModel' ) ) {
+			return false;
+		}
 		$this->_maps_model = new AgmMapModel;
-		if (!is_admin()) $this->_maps_codec = new AgmMarkerReplacer;
+		if ( ! is_admin() ) {
+			$this->_maps_codec = new AgmMarkerReplacer;
+		}
 	}
 
-	public function to_location ($data) {
+	public function to_location( $data ) {
 		$data = wp_parse_args($data, array(
 			self::KEY_ID => $this->_create_id(),
 			self::KEY_ADDRESS => '',
@@ -324,65 +318,76 @@ class App_Locations_MappedLocation extends App_Locations_Location {
 		$this->_data = $data;
 	}
 
-	public function to_storage () {
+	public function to_storage() {
 		return $this->_data;
 	}
 
-	public function to_map () {
-		$map_id = $this->get_var(self::KEY_GOOGLE_MAP_ID);
-		
-		if (!$map_id) $map_id = $this->_create_map();
-		if (!$map_id) return false; // Map creation failed
+	public function to_map() {
+		$map_id = $this->get_var( self::KEY_GOOGLE_MAP_ID );
 
-		$map = $this->_maps_model->get_map($map_id);
+		if ( ! $map_id ) {
+			$map_id = $this->_create_map();
+		}
+		if ( ! $map_id ) {
+			return false; // Map creation failed
+		}
+		$map = $this->_maps_model->get_map( $map_id );
 		return $map;
 	}
 
-	public function get_display_markup ($rich_content=true) {
+	public function get_display_markup( $rich_content = true ) {
 		return $rich_content
 			? $this->get_map()
-			: $this->get_address()
-		;
+			: $this->get_address();
 	}
 
-	public function get_admin_label () {
+	public function get_admin_label() {
 		return $this->get_address();
 	}
 
-	public function get_id () { return $this->get_var(self::KEY_ID); }
-	public function get_address () { return $this->get_var(self::KEY_ADDRESS); }
+	public function get_id() {
+		return $this->get_var( self::KEY_ID ); }
+	public function get_address() {
+		return $this->get_var( self::KEY_ADDRESS ); }
 
-	public function get_map () {
+	public function get_map() {
 		$map = $this->to_map();
-		if (!$map) {
-			$this->set_var(self::KEY_GOOGLE_MAP_ID, false);
-			do_action('app-locations-location-model-item_updated');
+		if ( ! $map ) {
+			$this->set_var( self::KEY_GOOGLE_MAP_ID, false );
+			do_action( 'app-locations-location-model-item_updated' );
 			return $this->get_map(); // Map got deleted somehow, let's double up
 		} else {
 			global $appointments;
 			$data = $appointments->options;
-			$overrides = !empty($data['google_maps']['overrides']) ? $data['google_maps']['overrides'] : array();
-			return $this->_maps_codec->create_tag($map, $overrides);
+			$overrides = ! empty( $data['google_maps']['overrides'] ) ? $data['google_maps']['overrides'] : array();
+			return $this->_maps_codec->create_tag( $map, $overrides );
 		}
 	}
 
-	private function _create_map () {
+	private function _create_map() {
+		if ( ! class_exists( 'AgmMapModel' ) ) {
+			return false;
+		}
 		$model = new AgmMapModel;
-		$map_id = $this->_maps_model->autocreate_map(null, null, null, $this->get_address());
-		if (!$map_id) return false;
-
-		$map = $this->_maps_model->get_map($map_id);
+		$map_id = $this->_maps_model->autocreate_map( null, null, null, $this->get_address() );
+		if ( ! $map_id ) {
+			return false;
+		}
+		$map = $this->_maps_model->get_map( $map_id );
 		$latitude = $longitude = false;
-		$position = !empty($map['markers'][0]['position']) ? $map['markers'][0]['position'] : false;
-		if (!empty($position)) {
+		$position = ! empty( $map['markers'][0]['position'] ) ? $map['markers'][0]['position'] : false;
+		if ( ! empty( $position ) ) {
 			$latitude = $position[0];
 			$longitude = $position[1];
 		}
-
-		$this->set_var(self::KEY_GOOGLE_MAP_ID, $map_id);
-		if ($latitude) $this->set_var(self::KEY_LAT, $latitude);
-		if ($longitude) $this->set_var(self::KEY_LNG, $longitude);
-		do_action('app-locations-location-model-item_updated');
+		$this->set_var( self::KEY_GOOGLE_MAP_ID, $map_id );
+		if ( $latitude ) {
+			$this->set_var( self::KEY_LAT, $latitude );
+		}
+		if ( $longitude ) {
+			$this->set_var( self::KEY_LNG, $longitude );
+		}
+		do_action( 'app-locations-location-model-item_updated' );
 		return $map_id;
 	}
 }
