055d16a66f468db1e745f886d7b16a232520dbf9
Panos <panos.lyrakis@gmail.com>
2018-07-21 22:54:27 +0300

    Use admin min time in inline edit

--- admin/views/inline-edit.php
+++ admin/views/inline-edit.php
@@ -73,16 +73,40 @@ $format = get_option( 'time_format' );
 $_start_time = $app_id ? strtotime( $app->get_start_time() ): '';
 $day_start = strtotime( $app->start );
 $slots = appointments_get_worker_weekly_start_hours( $app->service, $app->worker, $app->location, true );
+$admin_min_time = $options['admin_min_time'];
+
 if ( is_array( $slots ) ) {
-	foreach ( $slots as $slot ) {
-		$h = strtotime( $slot );
-		printf(
-			'<option value="%s" %s>%s</option>',
-			esc_attr( $slot ),
-			selected( $_start_time, $h, false ),
-			esc_html( date( $format, strtotime( $slot ) ) )
-		);
+
+	if ( ! empty( $admin_min_time ) ) {
+
+		$admin_min_time 	= $options['admin_min_time'];
+		$start_of_day 		= new DateTime( reset( $slots ) );
+		$end_of_day   		= new DateTime( end( $slots ) );
+		$interval 			= DateInterval::createFromDateString("{$admin_min_time} min");
+		$times    			= new DatePeriod($start_of_day, $interval, $end_of_day);
+
+	    foreach ($times as $time) {
+	        printf(
+	            '<option value="%s" %s>%s</option>',
+	            esc_attr( $time->format('H:i') ),
+	            selected( date( 'H:i', $_start_time ), $time->format('H:i'), false ),
+	            esc_html( $time->format('H:i') )
+	        );
+	    }
+
 	}
+	else{
+		foreach ( $slots as $slot ) {
+			$h = strtotime( $slot );
+			printf(
+				'<option value="%s" %s>%s</option>',
+				esc_attr( $slot ),
+				selected( $_start_time, $h, false ),
+				esc_html( date( $format, strtotime( $slot ) ) )
+			);
+		}
+	}
+	
 }
 ?>
 					</select>
